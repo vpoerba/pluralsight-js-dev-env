9e80ab8330253d56f6496120b07268e0e3cef859
Victor Poerba <v.poerba@jarvia.de>
2017-10-13 19:44:39 +0200

    Change space size from 2 into 4

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -2,17 +2,17 @@ var express = require('express');
 var path = require('path');
 var open = require('open');
 
-var port = 3000;
+var port = 3001;
 var app = express();
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
+    res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
 app.listen(port, function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    open('http://localhost:' + port);
-  }
+    if (err) {
+        console.log(err);
+    } else {
+        open('http://localhost:' + port);
+    }
 });
